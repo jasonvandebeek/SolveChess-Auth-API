6f4c23adb725cd29c2e4fe3ea43201f2b99a5211
jasonvandebeek <jasonvandebeek@gmail.com>
2024-01-17 15:08:21 +0100

    refactor: configurable cors

--- API/Program.cs
+++ API/Program.cs
@@ -44,10 +44,12 @@ builder.Services.AddScoped<HttpClient>();
 
 builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 
+string origins = Environment.GetEnvironmentVariable("SolveChess_CorsUrls") ?? throw new MissingEnvVariableException("No cors urls found in .env variables!");
+string[] originArray = origins.Split(';');
+
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowOrigin", builder => builder
-        .WithOrigins("https://localhost:3000", "https://solvechess.xyz")
+    options.AddPolicy("AllowOrigin", builder => builder.WithOrigins(originArray)
         .AllowAnyHeader()
         .AllowAnyMethod()
         .AllowCredentials());
